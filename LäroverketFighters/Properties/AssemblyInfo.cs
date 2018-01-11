5525f7d7b0658406db164699cddc3680302c0728
Bajjan <Bajjan@users.noreply.github.com>
2018-01-11 14:18:54 +0100

    New files

--- LäroverketFighters/Properties/AssemblyInfo.cs
+++ LäroverketFighters/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c33c375-555f-4118-bed4-007570a5e6ee")]
+[assembly: Guid("d2398be2-23be-4aac-a506-814738279141")]
 
 // Version information for an assembly consists of the following four values:
 //
