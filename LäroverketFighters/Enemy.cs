5467ccccdb3d741d1ba65a43db642a36ea9979fd
Bajjan <Bajjan@users.noreply.github.com>
2018-01-11 15:22:45 +0100

    Added enemy class

---
+++ LäroverketFighters/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LäroverketFighters
+{
+    //En class funkar som en beskrivning av någonting
+    //Här beskriver vi hur en fiende fungerar och
+    //vilka egenskaper den har
+    class Enemy
+    {
+        public int hp;
+        public int dmg;
+        public string name;
+
+        //Initialize, sätt värden på variablerna
+        public void Setup()
+        {
+            Random randomness = new Random();
+
+            hp = randomness.Next(5, 10);
+            dmg = randomness.Next(1, 5);
+
+            string[] namesToPick =
+            {
+                "Christina",//0
+                "Anton Larsson",//1
+                "Frimp",//2
+                "Gunnar",//3
+                "Boooose",//4
+                "Ringo",//5
+                "Bajjan"//6
+            };
+
+            name = namesToPick[randomness.Next(0, namesToPick.Length)];
+        }
+    }
+}
